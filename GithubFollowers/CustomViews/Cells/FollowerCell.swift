3df61caa27e25733238832a141e272b1a9ecd83f
Tunahan <tunahannaydinoglu@gmail.com>
2023-04-11 02:19:33 +0300

    use swiftUI with UIKit

--- GithubFollowers/CustomViews/Cells/FollowerCell.swift
+++ GithubFollowers/CustomViews/Cells/FollowerCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 class FollowerCell: UICollectionViewCell {
   static let reuseId = "FollowerCell"
@@ -30,8 +31,14 @@ class FollowerCell: UICollectionViewCell {
   }
 
   func set(follower: Follower){
-    usernameLabel.text = follower.login
-    avatarImageView.downloadImage(from: follower.avatarUrl)
+    if #available(iOS 16.0, *) {
+      contentConfiguration = UIHostingConfiguration{
+        FollowerView(follower: follower)
+      }
+    } else {
+      usernameLabel.text = follower.login
+      avatarImageView.downloadImage(from: follower.avatarUrl)
+    }
   }
 
   private func configure() {
